a594929f4f8502b6716443526c3873ac5fc9de52
mk_kim <mkkimtree.warevalley@gmail.com>
2018-01-05 15:51:39 +0900

    chap3 start code

---
+++ webstore/src/main/resources/db/sql/insert-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO PRODUCTS VALUES ('P1234', 'iPhone 6s', 'Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera','500','Apple','Smartphone','New',450,0,false);
+INSERT INTO PRODUCTS VALUES ('P1235', 'Dell Inspiron', 'Dell Inspiron 14-inch Laptop (Black) with 3rd Generation Intel Core processors',700,'Dell','Laptop','New',1000,0,false);
+INSERT INTO PRODUCTS VALUES ('P1236', 'Nexus 7', 'Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon¢â S4 Pro processor', 300,'Google','Tablet','New',1000,0,false);
+
+
+INSERT INTO CUSTOMERS VALUES('C1234', 'MK_KIM',  'INCHEON', true);
+INSERT INTO CUSTOMERS VALUES('C4567', 'MK_KIM2', 'SEOUL', false);
+INSERT INTO CUSTOMERS VALUES('C7890', 'MK_KIM3', 'HONGSUNG', true);
