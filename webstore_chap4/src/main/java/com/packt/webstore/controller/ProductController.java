f4764519e13c0737ec5a62f5f325d1ecd4da628a
mk_kim <mkkimtree.warevalley@gmail.com>
2018-01-05 15:53:21 +0900

    chap4 start code

---
+++ webstore_chap4/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.packt.webstore.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.ProductService;
+
+@Controller
+public class ProductController {
+//	@Autowired
+//	private ProductRepository productRepository;
+	@Autowired
+	private ProductService productService;
+	@RequestMapping("/products")
+	public String list(Model model) {
+		model.addAttribute("products", productService.getAllProducts());
+		return "products";
+	}
+	@RequestMapping("/update/stock")
+	public String updateStock(Model model) {
+		productService.updateAllStock();
+		return "redirect:/products";//위에 있는 list함수 호출
+	}
+}
